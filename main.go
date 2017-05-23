902f8e87288764f0905c9874c9c1690b16f34c33
versoul <mailto.versoul@gmail.com>
2017-05-23 19:32:51 +0300

    add parser
    Signed-off-by: versoul <mailto.versoul@gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"html/template"
+	"net/http"
 	"versoul/4lance/parser"
-	//"time"
 )
 
 var (
@@ -14,7 +14,7 @@ var (
 
 func main() {
 
-	prsr.Parse()
+	go prsr.Run()
 
 	r := gin.Default()
 	r.Static("/css", "./static/css")
@@ -33,18 +33,43 @@ func main() {
 	})*/
 	r.LoadHTMLFiles("./templates/base.html",
 		"./templates/header.html",
-		"./templates/error.html")
+		"./templates/main.html")
 	r.GET("/dashboard", func(c *gin.Context) {
+
+		session, err := mgo.Dial("localhost")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+
+		db := session.DB("4lance").C("projects")
+		// Query All
+		var results []map[string]interface{}
+		err = db.Find(nil).All(&results)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, v := range results {
+			if str, ok := v["projectPrice"].(string); ok {
+				v["projectPrice"] = template.HTML(str)
+			} else {
+				//LOL ITS NOT STRING!!
+			}
+
+		}
+
+		cnt, err := db.Find(nil).Count()
+		if err != nil {
+			panic(err)
+		}
+
 		c.HTML(http.StatusOK, "base", gin.H{
-			"Error": "Main website",
+			"Error":    "Main website",
+			"projects": results,
+			"count":    cnt,
 		})
 	})
 	//go r.Run() // listen and serve on 0.0.0.0:8080
 	r.Run()
-
-	/*ticker := time.NewTicker(1 * time.Minute)
-
-	for range ticker.C {
-		fmt.Println("TIME")
-	}*/
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,11 +1,14 @@
 package parser
 
 import (
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 	"regexp"
 	"strings"
 	"sync"
+	"time"
 )
 
 func checkErr(e error) {
@@ -15,7 +18,14 @@ func checkErr(e error) {
 }
 
 type singleton struct {
-	site string
+	site           string
+	dbHost         string
+	dbName         string
+	collectionName string
+}
+type Person struct {
+	Name  string
+	Phone string
 }
 
 var instance *singleton
@@ -24,38 +34,120 @@ var once sync.Once
 func GetInstance() *singleton {
 	once.Do(func() {
 		instance = &singleton{
-			site: "https://www.fl.ru",
+			site:           "https://www.fl.ru",
+			dbHost:         "localhost",
+			dbName:         "4lance",
+			collectionName: "projects",
 		}
 	})
 	return instance
 }
 
-func (self *singleton) Parse() {
+func (self *singleton) parse() {
 	doc, err := goquery.NewDocument(self.site + "/projects/?kind=1")
 	checkErr(err)
 	doc.Find("#projects-list div.b-post:not(.topprjpay)").Each(func(i int, s *goquery.Selection) {
+		session, err := mgo.Dial(self.dbHost)
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		c := session.DB(self.dbName).C(self.collectionName)
+
 		priceStr := s.Children().First().Text()
-		//TODO if not find id only!
 		link := s.Find("a.b-post__link")
 		projectTitle, err := link.Html()
 		checkErr(err)
 		projectHref, _ := link.Attr("href")
 
-		fmt.Println(projectTitle + "--" + projectHref)
-
-		var projectPrice string
-		if strings.Index(priceStr, "По договоренности") == -1 {
-			re := regexp.MustCompile(`>(.+)<`)
-			res := re.FindStringSubmatch(priceStr)
-			projectPrice = strings.TrimSpace(res[1])
-		}
-		fmt.Println(projectPrice)
-
 		re := regexp.MustCompile(`projects\/(.+)\/`)
 		projectId := re.FindStringSubmatch(projectHref)[1]
-		fmt.Println(projectId)
 
-		fmt.Println("***")
+		//Find doc with Id
+		cnt, err := c.Find(bson.M{"projectId": projectId}).Count()
+		if err != nil {
+			panic(err)
+		}
+
+		//If not exist
+		if cnt == 0 {
+			var projectPrice string
+			if strings.Index(priceStr, "По договоренности") == -1 {
+				re := regexp.MustCompile(`>(.+)<`)
+				res := re.FindStringSubmatch(priceStr)
+				projectPrice = strings.TrimSpace(res[1])
+			}
+
+			err = c.Insert(map[string]interface{}{
+				"site":         "fl",
+				"projectId":    projectId,
+				"projectTitle": projectTitle,
+				"projectHref":  projectHref,
+				"projectPrice": projectPrice,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			go self.parseOne(projectHref, projectId)
+		}
 	})
 
+}
+
+func (self *singleton) parseOne(url string, id string) {
+	doc, err := goquery.NewDocument(self.site + url)
+	checkErr(err)
+	descrElem := doc.Find("#projectp" + id)
+	projectDescription, err := descrElem.Html()
+	checkErr(err)
+	projectDescription = strings.TrimSpace(projectDescription)
+
+	catElem := descrElem.Next().Next()
+	dateString, err := catElem.Next().Children().Last().Html()
+	checkErr(err)
+
+	re := regexp.MustCompile(`\[(.+)\]`)
+	if re.MatchString(dateString) {
+		dateString = re.ReplaceAllString(dateString, "")
+	}
+	dateString = strings.TrimSpace(dateString)
+
+	layout := "02.01.2006 | 15:04"
+	projectDate, err := time.Parse(layout, dateString)
+	checkErr(err)
+
+	var projectCategories []string
+	catElem.ChildrenFiltered("a").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		re := regexp.MustCompile(`s\/(.+)\/`)
+		res := re.FindStringSubmatch(href)
+		category := strings.TrimSpace(res[1])
+		projectCategories = append(projectCategories, category)
+	})
+
+	session, err := mgo.Dial(self.dbHost)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB(self.dbName).C(self.collectionName)
+	query := bson.M{"projectId": id}
+	change := bson.M{"$set": bson.M{"projectDate": projectDate,
+		"projectDescription": projectDescription,
+		"projectCategories":  projectCategories,
+	},
+	}
+	err = c.Update(query, change)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (self *singleton) Run() {
+	ticker := time.NewTicker(1 * time.Minute)
+
+	for range ticker.C {
+		go self.parse()
+	}
 }
