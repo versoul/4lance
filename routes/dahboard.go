2227414cd08f549b6c630662fdd5a8887c95c5dc
versoul <mailto.versoul@gmail.com>
2017-05-30 19:22:43 +0300

    Signed-off-by: versoul <mailto.versoul@gmail.com>

--- routes/dahboard.go
+++ routes/dahboard.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	"errors"
 	"github.com/pressly/chi"
 	"gopkg.in/mgo.v2"
 	"html/template"
+	"net"
 	"net/http"
 	"strconv"
 	"time"
@@ -14,6 +16,47 @@ var (
 )
 
 type dashboardResource struct{}
+
+func init() {
+
+}
+
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
+}
 
 // Routes creates a REST router for the todos resource
 func (rs dashboardResource) Routes() chi.Router {
--- routes/routes.go
+++ routes/routes.go
@@ -5,14 +5,17 @@ import (
 	"github.com/pressly/chi"
 	"html/template"
 	"net/http"
+
 	"os"
 	"path/filepath"
 	"versoul/4lance/templateHelpers"
 )
 
 var (
-	hlprs = templateHelpers.GetHelpers()
+	hlprs   = templateHelpers.GetHelpers()
+	localIp = "192.168.1.91"
 )
+var EcternalIp string
 
 func minus(a, b int64) string {
 	fmt.Println(a)
@@ -31,14 +34,33 @@ func renderTemplate(w http.ResponseWriter, name string, pageData map[string]inte
 		},
 	}
 
-	var tpl = template.Must(template.New(name).Funcs(funcMap).ParseFiles(
+	templates := []string{
 		"./templates/base.html",
 		"./templates/header.html",
-		"./templates/"+name+".html"))
+	}
+
+	ip, err := externalIP()
+	if err != nil {
+		fmt.Println(err)
+	}
+	//Подклчаем метрики только на реальном сервере
+	if ip != localIp {
+		templates = append(templates, "./templates/metrics.html")
+	}
+	//Подключаем запрошеный шаблон
+	templates = append(templates, "./templates/"+name+".html")
+	//Парсим все шаблоны
+	var tpl = template.New(name).Funcs(funcMap)
+	for i := 0; i < len(templates); i++ {
+		tpl.ParseFiles(templates[i])
+	}
+	//Отображаем
 	tpl.ExecuteTemplate(w, "base", pageData)
 }
 func InitRoutes() {
 	r := chi.NewRouter()
+
+	r.Mount("/", staticRoutes())
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/dashboard/", 302)
@@ -46,11 +68,25 @@ func InitRoutes() {
 	r.Get("/dashboard", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/dashboard/", 302)
 	})
-	workDir, _ := os.Getwd()
-	filesDir := filepath.Join(workDir, "static")
-	r.FileServer("/static", http.Dir(filesDir))
 
 	r.Mount("/dashboard/", dashboardResource{}.Routes())
 
 	http.ListenAndServe(":8080", r)
+}
+func staticRoutes() chi.Router {
+	r := chi.NewRouter()
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "static")
+	r.FileServer("/static", http.Dir(filesDir))
+
+	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/favicon.ico")
+	})
+	r.Get("/google173377f79f6a476a.html", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/google173377f79f6a476a.html")
+	})
+	r.Get("/yandex_87613e29f1d00477.html", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/yandex_87613e29f1d00477.html")
+	})
+	return r
 }
